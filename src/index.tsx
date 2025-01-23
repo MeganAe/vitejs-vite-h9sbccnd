ae7964c4092ea731235f7b09e764103ce2c143e8
さ METOUSHELA. さ <168326500+MeganAe@users.noreply.github.com>
2025-01-23 20:07:32 +0200

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './main'; // Import directement le composant Home
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
