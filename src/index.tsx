a39302322cd3c35640c9f3ef22964c888f414895
さ METOUSHELA. さ <168326500+MeganAe@users.noreply.github.com>
2025-01-23 20:16:17 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './main'; // Import directement le composant Home
 
+import './index.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Home />
